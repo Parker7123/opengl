dc864f5ca4a5d67b393a933ae0b2700dd33277fc
Filip <fillipser143@gmail.com>
2022-12-28 16:10:54 +0100

    Shader class

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -4,6 +4,7 @@ import org.lwjgl.BufferUtils;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
 
+import java.io.IOException;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
@@ -15,31 +16,11 @@ import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class HelloWorld {
 
-    private static final String vertexShaderSource = """
-            #version 330 core
-            layout (location = 0) in vec3 aPos;
-            layout (location = 1) in vec4 aColor;
-            
-            out vec4 vertexColor;
-            void main()
-            {
-                gl_Position = vec4(aPos, 1.0);
-                vertexColor = aColor;
-            }
-            """;
+    private static final String vertexShaderSource = HelloWorld.class.getResource("shader.vert").getFile();
 
-    private static final String fragmentShaderSource = """
-            #version 330 core
-            out vec4 FragColor;
-            in vec4 vertexColor;
-            
-            void main()
-            {
-                FragColor = vertexColor;
-            }
-            """;
+    private static final String fragmentShaderSource = HelloWorld.class.getResource("shader.frag").getFile();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         // init
         GLFWErrorCallback.createPrint(System.err).set();
@@ -67,7 +48,7 @@ public class HelloWorld {
         int vertex_size = 3; // X, Y, Z,
         int color_size = 3; // R, G, B,
 
-        FloatBuffer vertex_data = BufferUtils.createFloatBuffer(vertices * vertex_size);
+        FloatBuffer vertex_data = BufferUtils.createFloatBuffer(vertices * (vertex_size + color_size));
         vertex_data.put(new float[] { -0.5f, -0.5f, 0f, 1.0f, 0.0f, 0.0f });
         vertex_data.put(new float[] { -0.5f, 0.5f, 0f, 0.0f, 1.0f, 0.0f});
         vertex_data.put(new float[] { 0.5f, -0.5f, 0f, 0.0f, 0.0f, 1.0f});
@@ -84,39 +65,7 @@ public class HelloWorld {
         indices.put(new int[] { 1, 2, 3 });
         indices.flip();
 
-        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader, vertexShaderSource);
-        glCompileShader(vertexShader);
-        IntBuffer success = BufferUtils.createIntBuffer(1);
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, success);
-        if(success.get() == 0)
-        {
-            System.out.println("error");
-        }
 
-        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragmentShader, fragmentShaderSource);
-        glCompileShader(fragmentShader);
-        success.rewind();
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, success);
-        if(success.get() == 0)
-        {
-            System.out.println("error");
-        }
-
-        int shaderProgram;
-        shaderProgram = glCreateProgram();
-        glAttachShader(shaderProgram, vertexShader);
-        glAttachShader(shaderProgram, fragmentShader);
-        glLinkProgram(shaderProgram);
-        success.rewind();
-        glGetShaderiv(shaderProgram, GL_LINK_STATUS, success);
-        if(success.get() == 0)
-        {
-            System.out.println("error");
-        }
-        glDeleteShader(vertexShader);
-        glDeleteShader(fragmentShader);
 
         int VAO = glGenVertexArrays();
         int VAO2 = glGenVertexArrays();
@@ -129,8 +78,10 @@ public class HelloWorld {
 //        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 //        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 12, 0); // stride 3 * sizeof(float)
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 24, 0); // stride 6 * sizeof(float)
         glEnableVertexAttribArray(0);
+        glVertexAttribPointer(1, 3, GL_FLOAT, false, 24, 12); // stride 6 * sizeof(float)
+        glEnableVertexAttribArray(1);
 
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
@@ -143,6 +94,8 @@ public class HelloWorld {
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindVertexArray(0);
 
+        Shader shader = new Shader(vertexShaderSource, fragmentShaderSource);
+
 //        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 //        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -154,11 +107,10 @@ public class HelloWorld {
             glClear(GL_COLOR_BUFFER_BIT);
 
             // triangles
-            glUseProgram(shaderProgram);
+            shader.use();
             double timeValue = glfwGetTime();
             float greenValue = ((float) (sin(timeValue)) / 2.0f) + 0.5f;
-            int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-            glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+            shader.set4Float("uniformColor", 0f, greenValue, 0f, 1f);
 
             glBindVertexArray(VAO);
             glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -173,7 +125,6 @@ public class HelloWorld {
 
         glDeleteVertexArrays(VAO);
         glDeleteBuffers(VBO);
-        glDeleteProgram(shaderProgram);
         glfwTerminate();
     }
 
---
+++ src/main/java/org/example/Shader.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import org.lwjgl.BufferUtils;
+
+import java.io.*;
+import java.nio.IntBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.lwjgl.opengl.GL33.*;
+
+public class Shader {
+    private final int shaderProgram;
+
+    // constructor reads and builds the shader
+    public Shader(String vertexPath, String fragmentPath) throws IOException {
+        String vertexShaderSource = Files.readString(new File(vertexPath).toPath());
+        String fragmentShaderSource = Files.readString(new File(fragmentPath).toPath());
+
+        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, vertexShaderSource);
+        glCompileShader(vertexShader);
+        IntBuffer success = BufferUtils.createIntBuffer(1);
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, success);
+        if(success.get() == 0)
+        {
+            System.out.println("error");
+        }
+
+        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, fragmentShaderSource);
+        glCompileShader(fragmentShader);
+        success.rewind();
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, success);
+        if(success.get() == 0)
+        {
+            System.out.println("error");
+        }
+
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        success.rewind();
+        glGetShaderiv(shaderProgram, GL_LINK_STATUS, success);
+        if(success.get() == 0)
+        {
+            System.out.println("error");
+        }
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+    }
+    // use/activate the shader
+    public void use(){
+        glUseProgram(shaderProgram);
+    }
+    // utility uniform functions
+    void setBool(String name, Boolean value) {
+        glUniform1i(glGetUniformLocation(shaderProgram, name), value.compareTo(false));
+    }
+    void setInt(String name, int value) {
+        glUniform1i(glGetUniformLocation(shaderProgram, name), value);
+    }
+    void setFloat(String name, float value) {
+        glUniform1f(glGetUniformLocation(shaderProgram, name), value);
+    }
+
+    void set4Float(String name, float v1, float v2, float v3, float v4) {
+        glUniform4f(glGetUniformLocation(shaderProgram, name), v1, v2, v3, v4);
+    }
+}
